06bfb6082be03803234ac8f6aa9415edcef9402d
OFF <peerapat13th@gmail.com>
2024-09-09 12:15:04 +0700

    product-get all product

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,34 +1,96 @@
 //Product Controller
 using DotnetStockAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotnetStockAPI.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 
-public class ProductController : ControllerBase{
+public class ProductController : ControllerBase
+{
     //DI > Object+Constructor
     private readonly ApplicationDbContext _context;
-    public ProductController(ApplicationDbContext context){
+    public ProductController(ApplicationDbContext context)
+    {
         _context = context;
     }
 
     //Endpont for Get All Product
     [HttpGet]
-    public ActionResult<product> getProducts(){
+    public ActionResult<product> getProducts(
+        [FromQuery] int page = 1,
+        [FromQuery] int limit = 2,
+        [FromQuery] string? searchQuery = null,
+        [FromQuery] int? selectedCategory = null
+    )
+    {
+        //skip is การข้าวข้อมูล
+        int skip = (page - 1) * limit;
         //LINQ stand for "Language Integrated Query" // select* from product
-        var products = _context.products.ToList();
+        //var products = _context.products.ToList();
+
+        //Get with condition
+        //var products = _context.products.Where(x => x.unitinstock>=10).ToList();
+
+        //Connect product to category
+        var query = _context.products
+        .Join(
+            _context.categories,
+            p => p.categoryid,
+            c => c.categoryid,
+            (p, c) => new
+            {
+                p.productid,
+                p.productname,
+                p.unitprice,
+                p.unitinstock,
+                p.productpicture,
+                p.categoryid,
+                p.createddate,
+                p.modifieddate,
+                c.categoryname
+            }
+        );
+
+        //If search Query
+        if (!string.IsNullOrEmpty(searchQuery))
+        {
+            query = query.Where(p => EF.Functions.ILike(p.productname!, $"%{searchQuery}%"));
+        }
+
+        //If selected Category
+        if (selectedCategory.HasValue){
+            query = query.Where(p=>p.categoryid==selectedCategory.Value);
+        }
+        //Count all products
+        var totalRecords = query.Count();
+
+        //Get products
+        var products = query
+        .Skip(skip)
+        .Take(limit)
+        .ToList();
+
         //Response to client as JSON
-        return Ok(products);
+        return Ok(
+            new
+            {
+                Total = totalRecords,
+                Products = products
+            }
+        );
     }
 
     //Endpont for get by ID
     [HttpGet("{id}")]
-    public ActionResult<product> getProduct(int id){
+    public ActionResult<product> getProduct(int id)
+    {
         //LINQ for get products by ID // slect * from product where id = id
         var product = _context.products.Find(id);
-        if (product == null){
+        if (product == null)
+        {
             return NotFound();
         }
         //Response to client as JSON
@@ -37,7 +99,8 @@ public class ProductController : ControllerBase{
 
     //Endpont for Create Product
     [HttpPost]
-    public ActionResult<product> CreateProduct([FromBody] product product){
+    public ActionResult<product> CreateProduct([FromBody] product product)
+    {
         //Add data in Products Table
         _context.products.Add(product);
         _context.SaveChanges();
@@ -48,11 +111,13 @@ public class ProductController : ControllerBase{
 
     //Endpont for Update Product
     [HttpPut("{id}")]
-    public ActionResult<product> UpdateProduct(int id,[FromBody] product product){
+    public ActionResult<product> UpdateProduct(int id, [FromBody] product product)
+    {
         //Find product by ID
         var productData = _context.products.Find(id); //select * from product where id =1
 
-        if (productData == null){
+        if (productData == null)
+        {
             return NotFound();
         }
 
@@ -65,7 +130,7 @@ public class ProductController : ControllerBase{
         productData.modifieddate = product.modifieddate;
 
         //Update data in Products Table
-        _context.SaveChanges(); 
+        _context.SaveChanges();
 
         //Response to client as JSON
         return Ok(productData);
@@ -73,18 +138,20 @@ public class ProductController : ControllerBase{
 
     //Endpont for Delete Product
     [HttpDelete("{id}")]
-    public ActionResult<product> DeleteProduct(int id){
+    public ActionResult<product> DeleteProduct(int id)
+    {
         //Find product by ID
         var product = _context.products.Find(id); //select * from product where id =1
 
-        if (product == null){
+        if (product == null)
+        {
             return NotFound();
         }
 
         //Delete data in Products Table
-        _context.products.Remove(product); 
+        _context.products.Remove(product);
         //Save Changes
-        _context.SaveChanges(); 
+        _context.SaveChanges();
         //Response to client as JSON
         return Ok(product);
     }
--- obj/Debug/net8.0/DotnetStockAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetStockAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetStockAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e223af597f0541f14412031994f17202f1d8b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b736bd1c331db409b0c93f80d2128df8aa813ea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetStockAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetStockAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
